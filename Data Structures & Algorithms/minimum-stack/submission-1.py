9c8ebf4b94bb4514434bc17676ce050d91f53a26
Wattanak Keo <wattanakkeo@gmail.com>
2026-04-14 20:07:21 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,19 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        smallest = self.stack[0]
+        for n in self.stack:
+            smallest = min(smallest, n)
+        return smallest
